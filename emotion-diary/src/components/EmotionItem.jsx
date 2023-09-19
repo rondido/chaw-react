5bcba98a06d323baf725aa50d743a20e65825eaf
rondido <rppr001@naver.com>
2023-09-20 00:47:51 +0900

    일기 작성하기 페이지

---
+++ emotion-diary/src/components/EmotionItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function EmotionItem({
+  emotion_id,
+  emotion_img,
+  emotion_descript,
+  onClick,
+  isSelected,
+}) {
+  return (
+    <div
+      className={[
+        "EmotionItem",
+        isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`,
+      ].join(" ")}
+      onClick={() => onClick(emotion_id)}
+    >
+      <img src={emotion_img} />
+      <span>{emotion_descript}</span>
+    </div>
+  );
+}
